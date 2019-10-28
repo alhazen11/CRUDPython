9f1cc2de9decf4b722dc68aca209bc7120f4d448
Vicky Sultan Ahmad <vickysultan08@gmail.com>
2019-10-28 09:36:55 +0700

    add user model and resources

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 class Config(object):
     # ...
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/yourdatabase'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/crudpython'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'change more secure'
-    JWT_SECRET_KEY = 'change more secure'
+    SECRET_KEY = 'crudpythoncrudpython'
+    JWT_SECRET_KEY = 'crudpythoncrudpython'
     JWT_BLACKLIST_ENABLED = True
     JWT_BLACKLIST_TOKEN_CHECKS = ['access', 'refresh']
     MYSQL_DATABASE_USER = 'root'
     MYSQL_DATABASE_PASSWORD = ''
-    MYSQL_DATABASE_DB = 'your database'
+    MYSQL_DATABASE_DB = 'crudpython'
     MYSQL_DATABASE_HOST = 'localhost'
     JSON_SORT_KEYS = False
